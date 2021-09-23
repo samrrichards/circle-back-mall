523711791f97e8283e4c34162978583118cff8d9
Sam Richards <samrichards@Sams-MacBook-Air-2.local>
2021-09-22 21:29:22 -0700

    generalized reduce functions

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,15 +1,11 @@
-const getTotalEntries = row => row.reduce((a, b) => a + b.entries, 0);
-
-const getTotalExits = row => row.reduce((a, b) => a + b.exits, 0);
-
-const getTotalOccupants = row => row.reduce((a, b) => a + b.occupancy, 0);
+const sumRowProperty = (row, property) => row.reduce((a, b) => a+b[property], 0);
 
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const getInitialDots = row => {
-  const totalEntries = getTotalEntries(row);
-  const totalExits = getTotalExits(row);
-  const totalOccupants = getTotalOccupants(row);
+  const totalEntries = sumRowProperty(row, 'entries');
+  const totalExits = sumRowProperty(row, 'exits');
+  const totalOccupants = sumRowProperty(row, 'occupants');
 
   const initialOccupants = totalOccupants + totalExits - totalEntries;
 
